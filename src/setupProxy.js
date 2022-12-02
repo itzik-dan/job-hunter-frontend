0baf1c35a6c810f2a6b834572b7871165982b251
itzik-dan <itzik0412@gmail.com>
2022-12-02 10:52:11 +0200

    change url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,14 +4,14 @@ module.exports = function (app) {
   app.use(
     "/auth/google",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "https://job-hunter-api.onrender.com",
       changeOrigin: true,
     })
   );
   app.use(
     "/api/**",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "https://job-hunter-api.onrender.com",
       changeOrigin: true,
     })
   );
