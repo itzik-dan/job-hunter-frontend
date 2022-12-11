e04878103564f1f7e9d78ac194f396023b6513a7
itzik-dan <itzik0412@gmail.com>
2022-12-12 00:10:31 +0200

    remove credentials

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-  withCredentials: true,
-  baseURL: `${
-    process.env.REACT_APP_BACKEND_URL || "http://localhost:5000"
-  }/api`,
+  baseURL: process.env.REACT_APP_BACKEND_URL
+    ? `${process.env.REACT_APP_BACKEND_URL}/api`
+    : "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
